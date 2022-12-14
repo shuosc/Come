710dea7f91b51bed23cd2b9b16c6f3d27ef99a70
longfangsong <longfangsong@icloud.com>
2022-12-14 21:14:38 +0800

    refactor and cleanup

--- src/ir/optimize/mod.rs
+++ src/ir/optimize/mod.rs
@@ -18,6 +18,10 @@ impl Optimizor {
     }
 
     pub fn optimize(self, mut ir: super::FunctionDefinition) -> super::FunctionDefinition {
+        // todo: make it a special `formalize` function
+        //       except fill name for the first block,
+        //       we should also fill the jump statement for blocks
+        //       which don't have a terminator
         if ir.content[0].name.is_none() {
             ir.content[0].name = Some(format!("{}_entry", ir.name));
         }
